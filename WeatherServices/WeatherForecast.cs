138eaeeec294460977f046ce3a8bcffb6cfb6ea8
Benjamin <Paminjo@tutanota.com>
2021-11-02 08:02:28 +0100

    Five Days Object is useable

--- WeatherServices/WeatherForecast.cs
+++ WeatherServices/WeatherForecast.cs
@@ -30,15 +30,20 @@ namespace WeatherServices
                 var IconPictureBoxes = new PictureBox[] { Icon_0, Icon_1, Icon_2, Icon_3, Icon_4 };
                 var ConditionLabels = new Label[] { Condition_0, Condition_1, Condition_2, Condition_3, Condition_4 };
                 var DetailsLabels = new Label[] { Details_0, Details_1, Details_2, Details_3, Details_4 };
-                var TempLabels = new Label[] { Temp_0, Temp_1, Temp_2, Temp_3, Temp_4 };
-                var TempMinLabels = new Label[] { TempMin_0, TempMin_1, TempMin_2, TempMin_3, TempMin_4 };
-                var TempMaxLabels = new Label[] { TempMax_0, TempMax_1, TempMax_2, TempMax_3, TempMax_4 };
+                var TempLabels = new Label[] { ValueTemp_0, ValueTemp_1, ValueTemp_2, ValueTemp_3, ValueTemp_4 };
+                var TempMinLabels = new Label[] { ValueMinTemp_0, ValueMinTemp_1, ValueMinTemp_2, ValueMinTemp_3, ValueMinTemp_4 };
+                var TempMaxLabels = new Label[] { ValueMaxTemp_0, ValueMaxTemp_1, ValueMaxTemp_2, ValueMaxTemp_3, ValueMaxTemp_4 };
 
                 for (int i = 0; i <= 4; i++)
                 {
-                    DateTimeLabels[i].Text = "WoW";
+                    DateTime day = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+                    DateTimeLabels[i].Text = day.AddSeconds(info.daily[i].dt).ToShortDateString();
+                    TempMinLabels[i].Text = Convert.ToString(info.daily[i].temp.min + " °C");
+                    TempMaxLabels[i].Text = Convert.ToString(info.daily[i].temp.max + " °C");
+                    TempLabels[i].Text = Convert.ToString((info.daily[i].temp.min + info.daily[i].temp.max)/2);
                     IconPictureBoxes[i].ImageLocation = "https://api.openweathermap.org/img/w/" + info.daily[i].weather[0].icon + ".png";
                 }
+
             }
         }
     }
