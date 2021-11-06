67a62ad9839a914a60f58a7679438de13371d3db
Paminjo <90216521+Paminjo@users.noreply.github.com>
2021-11-06 14:53:09 +0100

    comments added

--- WeatherServices/WeatherToday.cs
+++ WeatherServices/WeatherToday.cs
@@ -12,35 +12,45 @@ namespace WeatherServices
         {
             InitializeComponent();
 
+            // Thermometer-Grafik Einstellungen werden festgelegt;
             thermometer.Controls.Add(tempratureBar);
             tempratureBar.Height = 0;
             tempratureBar.Location = new Point(63, 430);
             tempratureBar.BackColor = Color.Transparent;
 
+            // Thermometer kriegt ein Steuerelement den BarFiller
             thermometer.Controls.Add(colorBarFiller);
             colorBarFiller.Height = 0;
             colorBarFiller.Location = new Point(63, 430);
             colorBarFiller.BackColor = Color.Transparent;
 
+            // Zwei Timer werden gestartet einer Für die Aktuelle Zeit der andere um Eigenschaften zu Überprüfen und
+            // dementsprechen zu agieren
             DateOnSystem.Text = DateTime.Now.ToShortDateString();
             timer1.Start();
             timer2.Start();
         }
 
+        // Erstellt benötigte Objekte
         private WeatherRequest _weatherRequest = new WeatherRequest();
         private WeatherForecast weatherForecast = new WeatherForecast();
 
+        // private Eigenschaften die genutzt werden für überprüfungen und daten übertragung/abgleich
         private double _lat;
         private double _lon;
         private bool _checkWeatherToday = true;
 
+        // Die Button_Click Methode 
         private async void btnSearch_Click(object sender, EventArgs e)
         {
+            
             await this.GetWeather();
         }
-
+        
+        // holt die Wetterdaten und setzt sie ein
         private async Task GetWeather()
         {
+            // Fehleranfälliger Code bzw. alles was Fehler verursachen könnte
             try
             {
                 var Info = await _weatherRequest.GetWeatherToday(TBCity.Text);
@@ -64,18 +74,21 @@ namespace WeatherServices
                 labPressure.Text = Info.main.pressure.ToString() + " mb";
                 ChangeTempInThermometer(Info.main.temp);
             }
+            // Fehler werden abgefangen und in einer Messagebox angezeigt -> man kann den schöner aussehen lassen, aber reicht mir
             catch (WebException e)
             {
                 MessageBox.Show(Convert.ToString(e));
             }
         }
 
+        // holt sich den Bild-String aus dem Internet
         private async Task<string> GetWeatherIconAsync(string iconString)
         {
             string icon = "https://api.openweathermap.org/img/w/" + iconString + ".png";
             return icon;
         }
 
+        // Ändert die Thermometer-Grafik
         private void ChangeTempInThermometer(double temp)
         {
             temp = temp - 273.15;
@@ -99,6 +112,7 @@ namespace WeatherServices
             return day;
         }
 
+        // TextBox Event KeyDown, wenn während der Bedienung der TextBox Enter gedrückt wird es ausgeführt
         private async void TBCity_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Enter)
@@ -107,7 +121,8 @@ namespace WeatherServices
             }
         }
 
-        private async void timer1_Tick(object sender, EventArgs e)
+        // Timer um im Millisekundentakt die Uhrezeit aus dem System zuholen
+        private void timer1_Tick(object sender, EventArgs e)
         {
             TimeOnSystem.Text = DateTime.Now.ToLongTimeString();
             if (TimeOnSystem.Text == "00:00:00")
@@ -117,6 +132,8 @@ namespace WeatherServices
 
         }
 
+        // Zweites Form bzw. das WeatherForecastForm wird sichtbar gemacht, welches man vorher erstellt hat
+        // Außerdem wird wenn eine Abfrage getätigt wurde, diese schon beim öffnen als Forecast auch ausgeführt
         private void OpenWeatherForecast_Click(object sender, EventArgs e)
         {
             this.Hide();
@@ -131,11 +148,13 @@ namespace WeatherServices
             _checkWeatherToday = false;
         }
 
+        // Wenn die Desktop Location von dieser Form geändert wird, übertragt dieser die Werte auf das weatherForecast Objekt, welches versteckt ist
         private void WeatherService_LocationChanged(object sender, EventArgs e)
         {
             weatherForecast.DesktopLocation = this.DesktopLocation;
         }
 
+        // Der Zweite Timer, dieseer wird für überprüfungen und datenabfragen zur untergeordneten Form verwendet
         private async void timer2_Tick(object sender, EventArgs e)
         {
             if (TBCity.Text != weatherForecast.cityName && weatherForecast.cityName != ""
